ee7d5d79df28b5a4402c9a0d0a74037d74426f67
Velky07 <jvvilela_pe@hotmail.com>
2022-12-04 13:44:32 -0300

    Pares

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+quantidade= 0
 
+for i in range(5):
+    a= int(input("digite um número:"))
+    if a%2 == 0:
+        quantidade=quantidade+1
+
+print("o usuário digitou", quantidade, "números pares." )
