ee9ce3f2ea00fc38bd4760f494da511977846987
Velky07 <jvvilela_pe@hotmail.com>
2022-12-13 23:40:48 -0300

    Bonificações Implementadas

--- bancoLib.py
+++ bancoLib.py
@@ -6,7 +6,7 @@ class Corrente():
         self.saldo = 0
 
     def deposito(self,valor):
-        self.saldo = self.saldo + valor
+        self.saldo = (self.saldo + valor) - (valor*0.001)
     
     def saque(self, valor):
         if self.saldo >= valor:
@@ -17,8 +17,22 @@ class Corrente():
 
 class Poupanca(Corrente):
 
-    def render(self):
-        self.saldo= self.saldo + (self.saldo*0.01)
+    def render(self, rent):
+        self.saldo= self.saldo + (self.saldo*(rent*0.01))
+
+class Bonificada(Corrente):   
+    def __init__(self, numConta):
+        super().__init__(numConta)
+        self.bonus = 0
+
+    def depositaComBonus(self, valor):
+        self.saldo = (self.saldo + valor) - (valor*0.001)
+        self.bonus += (valor*0.001)
+    
+    def renderBonus(self):
+        self.saldo = self.saldo + self.bonus
+        self.bonus = 0 
+
     
 class Banco():
     def __init__(self, nome):
@@ -29,7 +43,7 @@ class Banco():
         return self.nome
     
     def criarCorrente(self):
-        num = random.randint(0,10000)
+        num = random.randint(0, 1000)
         c= Corrente(num)
         self.contas.append(c)
         return num
@@ -40,26 +54,44 @@ class Banco():
         self.contas.append(p)
         return num
 
+    def criarBonificada(self):
+        num = random.randint(0, 1000)
+        b = Bonificada(num)
+        self.contas.append(b)
+        return num
+
     def consultaSaldo(self, numConta):
         for conta in self.contas:
             if conta.num==numConta:
                 return conta.saldo
         return -1
     
-    def depositar(self, numConta, grana):
+    def depositarComDesconto(self, numConta, valor):
         for conta in self.contas:
-            if conta.num==numConta:
-                conta.deposito(grana)
+            if conta.num == numConta and isinstance(conta, Bonificada):
+                conta.depositaComBonus(valor)
+                return True
+            elif conta.num == numConta and isinstance(conta, (Corrente or Poupanca)):
+                conta.deposito(valor)
+                return True  
+        return False
     
     def sacar(self, numConta, grana):
         for conta in self.contas:
             if conta.num==numConta:
                 return conta.saque(grana)
 
-    def renderPoupanca(self, numConta):
+    def renderPoupanca(self, numConta, rent):
         for conta in self.contas:
-            if conta.numero == numConta and isinstance(conta, Poupanca):
-                conta.render()
+            if conta.num == numConta and isinstance(conta, Poupanca):
+                conta.render(rent)
+                return True
+        return False
+
+    def renderBonus(self, numConta):
+        for i in self.contas:
+            if i.num == numConta and isinstance(i, Bonificada):
+                i.renderBonus()
                 return True
         return False
 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import bancoLib
+from bancoLib import Banco
 
 print("Bem vindo")
-bancoVelky= bancoLib.Banco("Belk Inc.")
+bancoUfrpe= Banco("Belk Inc.")
 print("Menu")
 print("1 - Criar nova conta")
 print("2 - Consultar Saldo da conta")
 print("3 - Depositar na conta")
 print("4 - Sacar da conta")
-print("5 - Deletar Conta")
+print("5 - Render Poupança")
+print("6 - Render bônus")
 print("0 - Sair")
 escolha= int(input("Digite a opção desejada:"))
 while escolha>0:
@@ -16,43 +17,56 @@ while escolha>0:
         print("Criando conta...")
         print("1 - Conta Corrente")
         print("2 - Conta Poupanca")
+        print("3 - Conta Bonificada")
         opcao = int(input("digite o tipo da conta:"))
         if opcao == 1:
-            n = bancoVelky.criarCorrente()
+            n = bancoUfrpe.criarCorrente()
+        elif opcao == 2:
+            n = bancoUfrpe.criarPoupanca()
         else:
-            n = bancoVelky.criarPoupanca()
+            n = bancoUfrpe.criarBonificada()
         print("Conta criada\nO número da conta é:",n)
     elif escolha==2:
         #Consultar Conta
         n= int(input("Digite o número da sua conta: "))
-        Saldo=bancoVelky.consultaSaldo(n)
+        Saldo=bancoUfrpe.consultaSaldo(n)
         print("O saldo da conta" , n , "é de: R$", Saldo)
     elif escolha==3:
         #Depositar na Conta
         n= int(input("Digite o número da sua conta: "))
         valor= float(input("Digite o valor a ser depositado: "))
-        Dep=bancoVelky.depositar(n, valor)
-        Saldo=bancoVelky.consultaSaldo(n)
+        Dep=bancoUfrpe.depositarComDesconto(n, valor)
+        Saldo=bancoUfrpe.consultaSaldo(n)
         print("Dinheiro Depositado!\nO saldo atual da conta" , n , "é de: R$", Saldo)
     elif escolha==4:
         #Sacar da Conta
         n= int(input("Digite o número da sua conta: "))
         valor= float(input("Digite o valor a ser sacado: "))
-        Sac= bancoVelky.sacar(n, valor)
-        Saldo= bancoVelky.consultaSaldo(n)
+        Sac= bancoUfrpe.sacar(n, valor)
+        Saldo= bancoUfrpe.consultaSaldo(n)
         if Sac:
             print("Dinheiro Sacado!")
         else:
             print("Saldo insuficiente...\n\n\n")
         print("O saldo atual da conta" , n , "é de: R$",Saldo)
     elif escolha == 5:
-        print("Rendendo Poupanca...")
-        n= int(input("digite o numero da conta poupanca:"))
-        rend= bancoVelky.renderPoupanca(n)
-        Saldo= bancoVelky.consultaSaldo(n)
+        n= int(input("Digite o numero da conta poupanca:"))
+        tratar= int(input("Digite quantos por cento sua poupança irá render:"))
+        rend= bancoUfrpe.renderPoupanca(n, tratar)
+        Saldo= bancoUfrpe.consultaSaldo(n)
         if rend:
             print("Poupanca com novo saldo")
         else:
             print("A conta não é poupanca ou não existe")
+        print("O saldo atual da conta" , n , "é de: R$", Saldo)
+    elif escolha == 6:
+        print("Rendendo Bônus...")
+        n = int(input("digite o numero da conta bonificada:"))
+        resp = bancoUfrpe.renderBonus(n)
+        Saldo= bancoUfrpe.consultaSaldo(n)
+        if resp:
+            print("Bonificada com novo saldo")
+        else:
+            print("A conta não é bonificada ou não existe")
         print("O saldo atual da conta" , n , "é de: R$", Saldo)
     escolha= int(input("Digite a opção desejada:"))
